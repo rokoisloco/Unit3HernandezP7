cf78179a08cddba877ab69d41e1e06ee3000f9e7
Byron Hernandez <739744@student.dusd.net>
2024-04-16 15:24:18 -0700

    Day 3

--- Unit3P7Hernandez/Assets/Course Library/Scripts/PlayerController.cs
+++ Unit3P7Hernandez/Assets/Course Library/Scripts/PlayerController.cs
@@ -8,11 +8,14 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
+    private Animator playerAnim;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -23,12 +26,21 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce (Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround=true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver=true;
+            Debug.Log("Game Over!");
+        }
        
     }
 }
